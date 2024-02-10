58b0279ce3473cda9d3686f516e73168f01c3182
Pablo <pabloc11b@gmail.com>
2024-02-10 13:12:38 -0300

    Modified: index.html, main.js, styles.css

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.querySelector('.menu-btn');
-    const menu = document.querySelector('.menu');
+    const menu = document.querySelector('.main-menu');
   
     menuBtn.addEventListener('click', () => {
       menu.classList.toggle('active');
