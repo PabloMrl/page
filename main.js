d22d435d2ff9232f65d744f1a5b0780b0fb59be4
Pablo <pabloc11b@gmail.com>
2024-02-10 01:04:38 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu = document.querySelector('.menu');
+  
+    menuBtn.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  });
+  
+ 
