971c9268bd39a2aff0fa8e116cf9950d96241a50
Pablo <pabloc11b@gmail.com>
2024-02-11 10:30:15 -0300

    realice cambios en index.html, style.css y js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//Funcion del menu hamburguesa
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.querySelector('.menu-btn');
     const menu = document.querySelector('.main-menu');
@@ -8,3 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
       menu.classList.toggle('active');
     });
   });
+
+// Mostrar u ocultar el botón cuando el usuario hace scroll
+window.addEventListener('scroll', () => {
+  const btnScrollToTop = document.getElementById('btnScrollToTop');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btnScrollToTop.style.display = 'block';
+  } else {
+      btnScrollToTop.style.display = 'none';
+  }
+});
+
+// Desplazarse hacia arriba cuando se hace clic en el botón
+document.getElementById('btnScrollToTop').addEventListener('click', () => {
+  document.body.scrollTop = 0; // Para navegadores que no soportan document.documentElement.scrollTop
+  document.documentElement.scrollTop = 0; // Para navegadores compatibles
+});
