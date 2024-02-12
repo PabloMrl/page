7bdff08a2c627c50ab839f72fdb01bdc1447f656
Pablo <pabloc11b@gmail.com>
2024-02-12 13:33:42 -0300

    scroll-snap

--- main.js
+++ main.js
@@ -7,28 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     menuBtn.addEventListener('click', () => {
       menu.classList.toggle('active');
     });
-    function scrollGallery() {
-      const gallery = document.querySelector('.gallery-container');
-      const galleryWidth = gallery.scrollWidth;
-      const viewportWidth = gallery.clientWidth;
-      const scrollLeft = gallery.scrollLeft;
-    
-      // Calcula la posición después de desplazar
-      let newPosition = scrollLeft + viewportWidth;
-    
-      // Si llega al final, vuelve al principio
-      if (newPosition >= galleryWidth) {
-          newPosition = 0;
-      }
-    
-      // Desplaza la galería
-      gallery.scroll({
-          left: newPosition,
-          behavior: 'smooth'
-      });
-    }
-    
-    setInterval(scrollGallery, 3000); // Desplaza la galería cada 3 segundos (3000 milisegundos)
     
   });
 
@@ -48,4 +26,25 @@ btnScrollToTop.addEventListener('click', () => {
   document.documentElement.scrollTop = 0; // Para navegadores compatibles
 });
 
+// function scrollGallery() {
+//   const gallery = document.querySelector('.gallery-container');
+//   const galleryWidth = gallery.scrollWidth;
+//   const viewportWidth = gallery.clientWidth;
+//   const scrollLeft = gallery.scrollLeft;
 
+//   // Calcula la posición después de desplazar
+//   let newPosition = scrollLeft + viewportWidth;
+
+//   // Si llega al final, vuelve al principio
+//   if (newPosition >= galleryWidth) {
+//       newPosition = 0;
+//   }
+
+//   // Desplaza la galería
+//   gallery.scroll({
+//       left: newPosition,
+//       behavior: 'smooth'
+//   });
+// }
+
+// setInterval(scrollGallery, 3000); // Desplaza la galería cada 3 segundos (3000 milisegundos)
